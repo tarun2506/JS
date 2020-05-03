6beb55fb17ba95563f139aaf5036c3d0ca849beb
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-05-03 20:28:50 +0530

    First JS

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Methord Number 1.
+// function checkDriverAge(){
+//     var age = prompt("What is your age?");
+
+// if (Number(age) < 18) {
+// 	alert("Sorry, you are too yound to drive this car. Powering off");
+// } else if (Number(age) > 18) {
+// 	alert("Powering On. Enjoy the ride!");
+// } else if (Number(age) === 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// }
+
+// }
+// checkDriverAge();
+
+
+// Methord Number 2
+// var checkDriverAge2 = function(){
+//     var age = prompt("What is your age?");
+
+//     if (Number(age) < 18) {
+//         alert("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         alert("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+// checkDriverAge2();
+
+
+// Methord Number 3
+// function checkDriverAge(a){
+//     if (Number(a) > 18){
+//         return "Powering On. Enjoy the ride!";
+//     } else if (Number(a) < 18){
+//         return "Sorry, you are too yound to drive this car. Powering off";
+//     } else if (Number(a) === 18){
+//         return "Congratulations on your first year of driving. Enjoy the ride!";
+//     }
+// }
+// alert(checkDriverAge());
