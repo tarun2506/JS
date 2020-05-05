109f8d594e3059c437803c12a786e96284e9f399
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-05-05 20:47:45 +0530

    For. while, do while and ForEach loops in JS

---
+++ loops in JS/script.js
@@ -0,0 +1,43 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "excercise",
+    "study javascript",
+    "eat healthy"
+];
+var todos1 = [
+    "clean room!",
+    "brush teeth!",
+    "excercise!",
+    "study javascript!",
+    "eat healthy!"
+];
+
+var todoslength = todos.length;
+
+// FOR LOOP
+// for (var i=0; i < todoslength; i++){                 
+//      todos.pop();
+// }
+
+// FOREACH LOOP
+// function abc(todo, i){
+//     console.log(todo, i)
+// }
+// todos.forEach(abc);
+// todos1.forEach(abc);
+
+// WHILE LOOP
+// var number = 0;
+// while (number < todoslength){
+//     todos[number] = todos[number] + "!"; 
+//     number++
+// }
+
+// DO WHILE LOOP
+// var number2 = 10;
+// do {
+//     console.log("hi tarun, Im do while looop");
+//     number2--;
+// } while (number2 > 0);
+
