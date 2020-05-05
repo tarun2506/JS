eaf6c6d7546d7ba0577f88f0f68030a0041d3970
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-05-05 22:00:55 +0530

    Finalized backend

--- Facebook/script.js
+++ Facebook/script.js
@@ -2,6 +2,14 @@ var database = [
     {
         username: "Tarun",
         password: "passwor0"
+    },
+    {
+        username: "Varun",
+        password: "passwor1"
+    },
+    {
+        username: "Karun",
+        password: "passwor3"
     }
 ];
 
@@ -13,18 +21,28 @@ var newsfeed = [
     {
         username: "Michal",
         timeline: "Javascript is sooooooooo cooool!"
+    },
+    {
+        username: "Bobby",
+        timeline: "Javascript is fun!"
     }
 ];
 
-var userName = prompt("Whats your username?");
-var password = prompt("Whats your password?");
+var username = prompt("What\'s your username?");
+var password = prompt("What\'s your password?");
 
-function signIn(username, pass){
-    if (username === database[0].username && pass === database[0].password){
-        console.log(newsfeed);
-    } else {
-        alert("Sorry, wrong username or password!");
+function signIn(username, password) {
+    for (var i = 0; i < database.length; i++) {
+        if (username === database[i].username) {
+            if (password === database[i].password) {
+                alert('Logged in');
+            } else {
+                alert('Incorrect password')
+            }
+            return;
+        }
     }
+    alert('User not found');
 }
 
-signIn(userName, password)
+signIn(username, password)
