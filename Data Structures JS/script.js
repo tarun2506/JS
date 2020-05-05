b4b9b2e5b0385dd840e982ad3468f67d3e75584a
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-05-05 17:45:23 +0530

    Datastructures

---
+++ Data Structures JS/script.js
@@ -0,0 +1,62 @@
+
+// ARRAYS:
+// var list = ["Banana", "Apples", "Oranges", "Blueberries"];
+// console.log(list);
+
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Bluesberries"]];
+//  console.log(array2[1][1][0]);
+ 
+// OBJECTS:
+// var user = {
+//     name: "Jhon",
+//     age: 34,
+//     country: "India",
+//     gender: "Male",
+//     isMarried: false,
+//     spells: ["sdsdsda", "sdsds", "sdsd"],
+//     shout: function(){
+//         console.log("Ahhhhhhhhhhhhhhh!!!!!!!!")
+//     }
+// };
+
+// var list = [
+//     {
+//         username: "Tarun",
+//         age: 18,
+//         sex: "male",
+//         class: 12,
+//         subject: "PCM + CS"
+//     },
+//     {
+//         username: "Varun",
+//         age: 17,
+//         sex: "male",
+//         class: 11,
+//         subject: "PCM + Hindi"
+//     }
+// ]
+var obj = {
+    name: "Tarun",
+    password: "12232"
+};
+
+var database = [
+    obj
+];
+
+var newsfeed = [
+    {
+        username: "Jhon", 
+        timeline: "So tired from all that learning!"
+    },
+
+    {
+        username: "Michel", 
+        timeline: "Javascript is sooooo cool!"
+    },
+
+    {
+        username: "Janny", 
+        timeline: "Javascript is preeetyy cool!"
+    }
+];
