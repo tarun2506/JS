aa67540741f36d68ee53191334fcdeb39fe4bae1
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-05-12 01:07:43 +0530

    Ternary operator

---
+++ Advance controlflow JS/index.js
@@ -0,0 +1,11 @@
+// Ternary operator: condition ? expr1 : expr2;
+
+// function uservalid(bool){
+//     return bool;
+// }
+
+// var a = uservalid(false) ? "User found" : "User not found";
+// var b = "Your acc number is " +  (uservalid(true) ? "312132" : "not allowed");
+
+
+
